b56b95448730f93e05f3fee03bac8d7fb8492f56
Michael Liao <askxuefeng@gmail.com>
2023-03-31 10:17:37 +0800

    add annotation for context.

---
+++ framework/summer-context/src/main/java/com/itranswarp/summer/annotation/Component.java
@@ -0,0 +1,19 @@
+package com.itranswarp.summer.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Component {
+
+    /**
+     * Bean name. Default to simple class name with first-letter-lowercase.
+     */
+    String value() default "";
+
+}
