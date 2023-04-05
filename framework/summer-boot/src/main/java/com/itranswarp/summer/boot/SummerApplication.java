f2ebd96e7d7e5c2cf779bc1b0501378a7601882c
Michael Liao <askxuefeng@gmail.com>
2023-04-05 11:02:51 +0800

    add summer-boot

---
+++ framework/summer-boot/src/main/java/com/itranswarp/summer/boot/SummerApplication.java
@@ -0,0 +1,74 @@
+package com.itranswarp.summer.boot;
+
+import java.io.File;
+import java.lang.management.ManagementFactory;
+import java.nio.file.Paths;
+import java.util.Set;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.Server;
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.itranswarp.summer.io.PropertyResolver;
+import com.itranswarp.summer.utils.ClassPathUtils;
+import com.itranswarp.summer.web.utils.WebUtils;
+
+public class SummerApplication {
+
+    static final String CONFIG_APP_YAML = "/application.yml";
+    static final String CONFIG_APP_PROP = "/application.properties";
+
+    final Logger logger = LoggerFactory.getLogger(SummerApplication.class);
+
+    public static void run(String webDir, String baseDir, Class<?> configClass, String... args) throws Exception {
+        new SummerApplication().start(webDir, baseDir, configClass, args);
+    }
+
+    public void start(String webDir, String baseDir, Class<?> configClass, String... args) throws Exception {
+        printBanner();
+
+        // start info:
+        final long startTime = System.currentTimeMillis();
+        final int javaVersion = Runtime.version().feature();
+        final long pid = ManagementFactory.getRuntimeMXBean().getPid();
+        final String user = System.getProperty("user.name");
+        final String pwd = Paths.get("").toAbsolutePath().toString();
+        logger.info("Starting {} using Java {} with PID {} (started by {} in {})", configClass.getSimpleName(), javaVersion, pid, user, pwd);
+
+        var propertyResolver = WebUtils.createPropertyResolver();
+        var server = startTomcat(webDir, baseDir, configClass, propertyResolver);
+
+        // started info:
+        final long endTime = System.currentTimeMillis();
+        final String appTime = String.format("%.3f", (endTime - startTime) / 1000.0);
+        final String jvmTime = String.format("%.3f", ManagementFactory.getRuntimeMXBean().getUptime() / 1000.0);
+        logger.info("Started {} in {} seconds (process running for {})", configClass.getSimpleName(), appTime, jvmTime);
+
+        server.await();
+    }
+
+    protected Server startTomcat(String webDir, String baseDir, Class<?> configClass, PropertyResolver propertyResolver) throws Exception {
+        int port = propertyResolver.getProperty("${server.port:8080}", int.class);
+        logger.info("starting Tomcat at port {}...", port);
+        Tomcat tomcat = new Tomcat();
+        tomcat.getConnector().setPort(port);
+        Context ctx = tomcat.addWebapp("", new File(webDir).getAbsolutePath());
+        WebResourceRoot resources = new StandardRoot(ctx);
+        resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes", new File(baseDir).getAbsolutePath(), "/"));
+        ctx.setResources(resources);
+        ctx.addServletContainerInitializer(new SummerServletContainerInitializer(configClass, propertyResolver), Set.of());
+        tomcat.start();
+        logger.info("Tomcat started at port {}...", port);
+        return tomcat.getServer();
+    }
+
+    protected void printBanner() {
+        String banner = ClassPathUtils.readString("/banner.txt");
+        banner.lines().forEach(System.out::println);
+    }
+}
---
+++ framework/summer-boot/src/main/java/com/itranswarp/summer/boot/SummerServletContainerInitializer.java
@@ -0,0 +1,46 @@
+package com.itranswarp.summer.boot;
+
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.itranswarp.summer.context.AnnotationConfigApplicationContext;
+import com.itranswarp.summer.context.ApplicationContext;
+import com.itranswarp.summer.io.PropertyResolver;
+import com.itranswarp.summer.web.ServletContextPostProcessor;
+import com.itranswarp.summer.web.utils.WebUtils;
+
+import jakarta.servlet.ServletContainerInitializer;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+
+public class SummerServletContainerInitializer implements ServletContainerInitializer {
+
+    final Logger logger = LoggerFactory.getLogger(getClass());
+    final Class<?> configClass;
+    final PropertyResolver propertyResolver;
+
+    public SummerServletContainerInitializer(Class<?> configClass, PropertyResolver propertyResolver) {
+        this.configClass = configClass;
+        this.propertyResolver = propertyResolver;
+    }
+
+    @Override
+    public void onStartup(Set<Class<?>> c, ServletContext ctx) throws ServletException {
+        logger.info("Servlet container start. ServletContext = {}", ctx);
+
+        String encoding = propertyResolver.getProperty("${summer.web.character-encoding:UTF-8}");
+        ctx.setRequestCharacterEncoding(encoding);
+        ctx.setResponseCharacterEncoding(encoding);
+
+        ServletContextPostProcessor.setServletContext(ctx);
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(this.configClass, this.propertyResolver);
+        logger.info("Application context created: {}", applicationContext);
+
+        // register DispatcherServlet:
+        WebUtils.registerDispatcherServlet(ctx, this.propertyResolver);
+        // register filters:
+        WebUtils.registerFilters(ctx);
+    }
+}
